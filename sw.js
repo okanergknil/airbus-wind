6e5e109419f663001ffc4db6ed823e7201bb2a83
okanergknil <72084461+okanergknil@users.noreply.github.com>
2026-03-28 12:17:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,34 @@
-const CACHE_NAME = "airbus-wind-v1";
+const CACHE_NAME = "airbus-wind-v2"; // Versiyon 2!
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json"
 ];
 
-// Uygulamayı cihaza kur ve dosyaları önbelleğe al
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
 });
 
-// İnternet yoksa bile cihazdaki kayıtlı dosyaları aç
+// Eski versiyon önbellekleri silme kodu
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
